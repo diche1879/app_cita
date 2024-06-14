8fb5719d24390d99eac159bad7549d109c16d9d3
diche1879 <147421870+diche1879@users.noreply.github.com>
2024-06-14 11:17:30 +0200

    plantillas html y base de datos

---
+++ consulado_app/app.js
@@ -0,0 +1,9 @@
+//Requiero los modulos que necesito
+const express = require('express');
+const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
+
+//inicializar express
+const app = express();
+
+//configurar dotenv
